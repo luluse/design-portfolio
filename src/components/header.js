5243db3ff524bf1acb66eed2966c4d96021450c0
LULU SEVIGNON <LUDIVINE.SEVIGNON@GMAIL.COM>
2021-01-14 15:49:27 -0800

    added background to header when scrolling

--- src/components/header.js
+++ src/components/header.js
@@ -4,24 +4,52 @@ import React from "react"
 import logo from '../images/logo-designcode.svg';
 import './Header.css'
 
-const Header = ({ siteTitle }) => (
-  <div className="Header">
-  <div className="HeaderGroup">
-    <Link to="/"><img src={logo} width="30" /></Link>
-    <Link to="/courses">Courses</Link>
-    <Link to="/downloads">Downloads</Link>
-    <Link to="/workshops">Workshops</Link>
-    <Link to="/buy"><button>Buy</button></Link>
-  </div>
-</div>
-)
 
-Header.propTypes = {
-  siteTitle: PropTypes.string,
+class Header extends React.Component {
+
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      hasScrolled: false
+    }
+  }
+
+  componentDidMount() {
+    window.addEventListener('scroll', this.handleScroll)
+  }
+
+  handleScroll = (event) => {
+    const scrollTop = window.pageYOffset
+
+    if (scrollTop > 50) {
+      this.setState({ hasScrolled: true })
+    } else {
+      this.setState({ hasScrolled: false })
+    }
+  }
+
+  render() {
+    return (
+      <div className={this.state.hasScrolled ? 'Header HeaderScrolled' : 'Header'}>
+        <div className="HeaderGroup">
+          <Link to="/"><img src={logo} width="30" /></Link>
+          <Link to="/courses">Courses</Link>
+          <Link to="/downloads">Downloads</Link>
+          <Link to="/workshops">Workshops</Link>
+          <Link to="/buy"><button>Buy</button></Link>
+        </div>
+      </div>
+    )
+  }
 }
 
-Header.defaultProps = {
-  siteTitle: ``,
-}
+// Header.propTypes = {
+//   siteTitle: PropTypes.string,
+// }
+
+// Header.defaultProps = {
+//   siteTitle: ``,
+// }
 
 export default Header
