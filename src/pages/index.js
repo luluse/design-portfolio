b9ec7bebf442c0075b91473e2cfdfc490ef3ca53
LULU SEVIGNON <LUDIVINE.SEVIGNON@GMAIL.COM>
2021-01-13 20:32:20 -0800

    added initial data to homepage

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,14 +8,17 @@ import SEO from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
+    <div>
+      <div className="Hero">
+        <div className="HeroGroup">
+          <h1>Hi, I'm Lulu</h1>
+          <p>Welcome to my Portfolio</p>
+          <Link to="/page-2/">Go to page 2</Link>
+          <br /><br />
+          <Link to="/video">Watch the video</Link>
+        </div>
+      </div>
     </div>
-    <Link to="/page-2/">Go to page 2</Link> <br />
-    <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
   </Layout>
 )
 
