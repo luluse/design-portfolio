ec97357640464058225c7c7e9c7aad9db5f477d6
LULU SEVIGNON <LUDIVINE.SEVIGNON@GMAIL.COM>
2021-01-18 12:18:52 -0800

    added stripe payment

---
+++ functions/stripe-charge.js
@@ -0,0 +1,28 @@
+require("dotenv").config({
+  path: `.env.${process.env.NODE_ENV}`,
+})
+
+const stripe = require('stripe')(process.env.GATSBY_STRIPE_SECRET)
+
+exports.handler = async function (event) {
+  const {
+    tokenId,
+    email,
+    name,
+    description,
+    amount
+  } = JSON.parse(event.body)
+
+  const customer = await stripe.customers.create({
+    description: email,
+    source: tokenId
+  })
+
+  await stripe.charges.create({
+    customer: customer.id,
+    amount,
+    name,
+    description,
+    currency: 'usd'
+  })
+}
